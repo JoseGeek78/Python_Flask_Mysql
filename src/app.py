d77f835e999a50675fb026a00ab573c153c69590
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-14 19:28:58 +0100

    Updated

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'src', 'templates')
 
 app = Flask(__name__, template_folder = template_dir)
+
+
+# Rutas de la aplicación
+@app.route('/')
+def home():
+    return render_template('index.html')
