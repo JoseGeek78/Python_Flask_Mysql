82392b5a95c8ff38bba843f844e6dda42b273207
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-14 06:25:41 +0100

    Updated

--- src/app.py
+++ src/app.py
@@ -2,3 +2,6 @@ from flask import Flask
 import os
 
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src', 'templates')
+
+app = Flask(__name__, template_folder = template_dir)
