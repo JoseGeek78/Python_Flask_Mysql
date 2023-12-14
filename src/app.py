4d52e11819125a85b5158259558081a70e3b7c81
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-14 19:33:19 +0100

    Updated

--- src/app.py
+++ src/app.py
@@ -11,3 +11,7 @@ app = Flask(__name__, template_folder = template_dir)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
